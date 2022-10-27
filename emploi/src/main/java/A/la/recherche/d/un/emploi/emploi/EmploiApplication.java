c7bc09f3f6acbd3d066d25e180607ac16a128abd
amil sebo <amil.sebo@uha.fr>
2022-10-27 11:03:26 +0200

    Ajout 6 candidats pour 3 annonces

--- emploi/src/main/java/A/la/recherche/d/un/emploi/emploi/EmploiApplication.java
+++ emploi/src/main/java/A/la/recherche/d/un/emploi/emploi/EmploiApplication.java
@@ -38,9 +38,9 @@ public class EmploiApplication {
 			candidat Jean = new candidat("Jacques","Jean","jean@jacques.fr",1);
 			candidat Cerise = new candidat("Josseaume","Cerise","cerise@josseaume.fr",1);
 			candidat Alexandre = new candidat("St-Jean","Alexandre","alexandre@st-jean.fr",1);
-			candidat Édouard = new candidat("Huard","Édouard","edouard@huard.fr",1);
-			candidat Camille = new candidat("Trudeau","Camille","camille@trudeau.fr",1);
-			candidat Orva = new candidat("Meilleur","Orva","ovra@meilleur.fr",1);
+			candidat Édouard = new candidat("Huard","Édouard","edouard@huard.fr",2);
+			candidat Camille = new candidat("Trudeau","Camille","camille@trudeau.fr",2);
+			candidat Orva = new candidat("Meilleur","Orva","ovra@meilleur.fr",3);
 
 			
 
@@ -55,23 +55,23 @@ public class EmploiApplication {
 			annoncedao.save(annonce1);
 
 
-			// Annonce 2
-			// ArrayList<candidat> liste2 = new ArrayList<>();
-			// liste2.add(Édouard);
-			// liste2.add(Camille);
+			//Annonce 2
+			ArrayList<candidat> liste2 = new ArrayList<>();
+			liste2.add(Édouard);
+			liste2.add(Camille);
 
 
-			// annonce annonce2 = new annonce("Entreprise B", "Développeur Back End", "France", "Aimeriez-vous un rôle qui peut vraiment faire ressortir votre créativité ? Nous sommes à la recherche d'une personne spéciale pour se joindre à notre équipe et apporter cette touche d'innovation. Comme nous réalisons quelque chose que personne n'a jamais fait auparavant, nous avons besoin d'aide pour une multitude de sujets.", "Stage", 0,liste2);
-			// annoncedao.save(annonce2);
+			annonce annonce2 = new annonce("Entreprise B", "Développeur Back End", "France", "Aimeriez-vous un rôle qui peut vraiment faire ressortir votre créativité ? Nous sommes à la recherche d'une personne spéciale pour se joindre à notre équipe et apporter cette touche d'innovation. Comme nous réalisons quelque chose que personne n'a jamais fait auparavant, nous avons besoin d'aide pour une multitude de sujets.", "Stage", 0,liste2);
+			annoncedao.save(annonce2);
 
-			// // Annonce 3
-			// ArrayList<candidat> liste3 = new ArrayList<>();
-			// liste2.add(Orva);
+			// Annonce 3
+			ArrayList<candidat> liste3 = new ArrayList<>();
+			liste2.add(Orva);
 
 
-			// annonce annonce3 = new annonce("Entreprise C", "Développeur Front End", "France", "Vous vivez et respirez votre travail ? Si c'est le cas, nous sommes à la recherche d'un Développeur Full Stack de génie. Comme une équipe d'aventuriers se forgeant leur chemin vers un nouveau continent, nous nous frayons un chemin vers l'inconnu... Il ne serait pas trop exagéré de dire que nous révolutionnons le marché avec notre solution innovatrice et pratique. Nous sommes à la recherche d'un visage frais pour rejoindre notre équipe en pleine croissance. Vous aurez besoin d'être un expert avec une solide connaissance en JavaScript et un vif intérêt pour Python.", "CDI", 0,liste3);
+			annonce annonce3 = new annonce("Entreprise C", "Développeur Front End", "France", "Vous vivez et respirez votre travail ? Si c'est le cas, nous sommes à la recherche d'un Développeur Full Stack de génie. Comme une équipe d'aventuriers se forgeant leur chemin vers un nouveau continent, nous nous frayons un chemin vers l'inconnu... Il ne serait pas trop exagéré de dire que nous révolutionnons le marché avec notre solution innovatrice et pratique. Nous sommes à la recherche d'un visage frais pour rejoindre notre équipe en pleine croissance. Vous aurez besoin d'être un expert avec une solide connaissance en JavaScript et un vif intérêt pour Python.", "CDI", 0,liste3);
 
-			// annoncedao.save(annonce3);			
+			annoncedao.save(annonce3);			
 		};
 	}
 }
